cc1b8343be197ed24100d6ff2722cd1c138bdad6
Ann Marie Mahon <AnnMarie.Mahon@colorado.edu>
2019-10-17 15:18:33 -0600

    Add Google vr, physics box, sensor

---
+++ UnityFolder/TAM-NotPortal/Assets/DetectCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    MeshRenderer render;
+    void Start()
+    {
+        render = this.GetComponent<MeshRenderer>();
+        render.material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        //MeshRender 
+        render.material.color = Color.green;
+    }
+
+    void OnTriggerExit(Collider other){
+        render.material.color = Color.red;
+    }
+}
