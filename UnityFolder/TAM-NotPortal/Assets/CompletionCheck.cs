d83d5219406bf9103397111b337450f97e446295
Ann Marie Mahon <AnnMarie.Mahon@colorado.edu>
2019-10-22 10:47:23 -0600

    Add completion check, new skybox.  Update gitignore and collision check

---
+++ UnityFolder/TAM-NotPortal/Assets/CompletionCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//#include "DetectCollision.cs"
+//<include file='DetectCollision.cs'/>
+public class CompletionCheck : MonoBehaviour
+{
+    // GameObject other;
+    // Component other1;
+    public DetectCollision toCheck;
+    public DetectCollision toCheck2;
+    // toCheck.GetComponent<DetectCollision>().get_collided();
+    MeshRenderer render;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = this.GetComponent<MeshRenderer>();
+        render.material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(toCheck.get_collided() && toCheck2.get_collided()){
+            render.material.color = Color.green;
+        }
+        else{
+            render.material.color = Color.red;
+        }
+    }
+}
