353c79bcce2f6dd92368f4fe0b60f9238c535974
Ann Marie Mahon <AnnMarie.Mahon@colorado.edu>
2019-10-29 16:38:08 -0600

    Actually add grabber

---
+++ UnityFolder/TAM-NotPortal/Assets/Grabber.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour
+{
+    private Collider grabbedGameObject;
+    //GameObject hand = this.getComponent<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetButtonDown("Fire1")){
+            if(grabbedGameObject != null){
+                grabbedGameObject.transform.parent = transform;
+                grabbedGameObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+            //buttonHeld = true;
+        }
+
+        if(Input.GetButtonUp("Fire1")){
+            grabbedGameObject.transform.parent = null;
+            grabbedGameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }   
+    }
+
+    void OnTriggerEnter(Collider other){
+        //MeshRender 
+        if(other.gameObject.tag == "box"){
+            grabbedGameObject = other;
+            // grabbedGameObject.transform.parent = hand.transform;
+            // grabbedGameObject.getComponent<Rigidbody>().isKinematic = true;
+            //collided++;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        grabbedGameObject = null;
+    }
+}
